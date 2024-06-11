60396cbb1240e53db0e0d5e7524bd0a68f667d78
acortez <cortez.allencolocado@gmail.com>
2024-06-11 14:23:34 +0800

    workshop 5

--- tests/workshop_4/workshop_4.spec.ts
+++ tests/workshop_4/workshop_4.spec.ts
@@ -1,3 +1,65 @@
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
-test("Workshop 4", async ({ page }) => {});
+test("Workshop 4 - Handling alerts", async ({ page }) => {
+  await page.goto(
+    "file:///C:/Users/arc/Desktop/Projects/playwright-exercise/tests/workshop_4/index.html"
+  );
+  let alertMessage;
+  //This deals with Pop-ups and alerts
+  page.on("dialog", async (dialog) => {
+    expect(dialog.type()).toBe("alert");
+    alertMessage = dialog.message();
+    await dialog.accept();
+  });
+  await page.click("//button[@id='show-alert']");
+  expect(alertMessage).toBe("This is a simple alert.");
+  console.log(alertMessage);
+});
+
+test("Dismissing alerts", async ({ page }) => {
+  await page.goto(
+    "file:///C:/Users/arc/Desktop/Projects/playwright-exercise/tests/workshop_4/index.html"
+  );
+
+  let alertMessage;
+  //This deals with Pop-ups and alerts
+  page.on("dialog", async (dialog) => {
+    alertMessage = dialog.message();
+    await dialog.dismiss();
+  });
+  await page.click("//button[@id='show-confirm']");
+
+  expect(alertMessage).toBe("You clicked Cancel.");
+  console.log(alertMessage);
+});
+
+test("Confirming alerts", async ({ page }) => {
+  await page.goto(
+    "file:///C:/Users/arc/Desktop/Projects/playwright-exercise/tests/workshop_4/index.html"
+  );
+
+  let alertMessage;
+  //This deals with Pop-ups and alerts
+  page.on("dialog", async (dialog) => {
+    alertMessage = dialog.message();
+    await dialog.accept();
+  });
+  await page.click("//button[@id='show-confirm']");
+
+  expect(alertMessage).toBe("You clicked OK.");
+  console.log(alertMessage);
+});
+
+test.only("Pop-ups", async ({ page }) => {
+  await page.goto(
+    "file:///C:/Users/arc/Desktop/Projects/playwright-exercise/tests/workshop_4/index.html"
+  );
+  const [popup] = await Promise.all([
+    page.waitForEvent("popup"),
+    page.click("//button[@id='open-popup']"),
+  ]);
+
+  await popup.waitForLoadState();
+
+  await popup.close();
+});
